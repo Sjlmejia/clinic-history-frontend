3c36df5683f2c120cb9a93cbfa93364f2e61094b
Jorge Mejia <jorgemejia2304@gmail.com>
2020-08-04 17:13:52 -0500

    add list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
+import { AppRouting } from './routes/routing';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { ListComponent } from './components/list/list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule} from '@angular/material/card';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    ListComponent,
   ],
   imports: [
-    BrowserModule
+    AppRouting,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
